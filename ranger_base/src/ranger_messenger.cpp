b25141de1797b4d317d7678f644a675a735e2163
Sebastian Ohl <s.ohl@ostfalia.de>
2026-07-01 11:03:33 +0000

    fix unused

--- ranger_base/src/ranger_messenger.cpp
+++ ranger_base/src/ranger_messenger.cpp
@@ -536,10 +536,10 @@ double RangerROSMessenger::CalculateSteeringAngle(geometry_msgs::msg::Twist msg,
   radius = linear / angular;
   int k = (msg.angular.z * msg.linear.x) >= 0 ? 1.0 : -1.0;
 
-  double l, w, phi_i, x;
+  double l, /*w,*/ phi_i/*, x*/;
   l = robot_params_.wheelbase;
-  w = robot_params_.track;
-  x = sqrt(radius * radius + (l / 2) * (l / 2));
+  // w = robot_params_.track;
+  // x = sqrt(radius * radius + (l / 2) * (l / 2));
   // phi_i = atan((l / 2) / (x - w / 2));
   phi_i = atan((l / 2) / radius);
   return k * phi_i;
